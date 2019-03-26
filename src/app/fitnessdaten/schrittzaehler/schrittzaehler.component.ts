58e54b4bda7fcd266f970845fabfd53d819216fd
Ida Gouleu <idagoule75@yahoo.fr>
2019-03-26 16:01:28 +0100

    Fix JSONStringify to Key-Value in SchrittzaehlerComponent.ts

--- src/app/fitnessdaten/schrittzaehler/schrittzaehler.component.ts
+++ src/app/fitnessdaten/schrittzaehler/schrittzaehler.component.ts
@@ -2,6 +2,8 @@ import { Component, NgZone } from "@angular/core";
 import { alert } from "tns-core-modules/ui/dialogs";
 import { AggregateBy, HealthData, HealthDataType } from "nativescript-health-data";
 
+import { getLocaleDateFormat } from "@angular/common";
+
 @Component({
     selector: 'ns-schrittzaehler',
     templateUrl: './schrittzaehler.component.html',
@@ -20,18 +22,31 @@ export class SchrittzaehlerComponent {
     
       private healthData: HealthData;
       resultToShow = "";
+
     
       constructor(private zone: NgZone) {
+       
         this.healthData = new HealthData();
       }
-    
+
+      /*private round(value: number, decimals: number) {
+        var strnumber = `${value}e${decimals}`;
+        var numround = Math.round(Number(strnumber));
+        var strfinal = `${numround}e-${decimals}`;
+        return Number(strfinal);
+      }*/
+
+      private formatDate(value: Date) {
+        return value.toLocaleDateString("en-us") + " " + value.toLocaleTimeString("en-us");
+      }
+      
       isAvailable(): void {
         this.healthData.isAvailable(true)
-            .then(available => this.resultToShow = available ? "Health Data available" : "Health Data not available :(");
+            .then(available => this.resultToShow = available ? "Health Data available :)" : "Health Data not available :(");
       }
     
       isAuthorized(): void {
-        this.healthData.isAuthorized([<HealthDataType>{name: "weight", accessType: "read"}])
+        this.healthData.isAuthorized([<HealthDataType>{name: "steps", accessType: "read"}])
             .then(authorized => setTimeout(() => alert({
               title: "Authentication result",
               message: (authorized ? "" : "Not ") + "authorized for " + JSON.stringify(SchrittzaehlerComponent.TYPES),
@@ -49,24 +64,43 @@ export class SchrittzaehlerComponent {
             .catch(error => console.log("Request auth error: ", error));
       }
     
+     /* getAllData():void {
+        Object.keys(this.healthData).forEach(function(key) {
+                  
+          console.log('Key : ' + key + ', Value : ' + this.healthData[key])
+        })
+      }*/
       getData(dataType: string, unit: string, aggregateBy?: AggregateBy): Promise<void> {
+        
         return this.healthData.query(
             {
-              startDate: new Date(new Date().getTime() - 3 * 24 * 60 * 60 * 1000), // 3 day ago
+              startDate: new Date(new Date().getTime() - 3 * 24 * 60 * 60 * 1000), // 1 day ago
               endDate: new Date(), // now
               dataType: dataType, // equal to the 'name' property of 'HealthDataType' ("steps,weight etc...")
               unit: unit,  // make sure this is compatible with the 'dataType' (see below) ("count") 
               aggregateBy: aggregateBy,  // optional, one of: "hour", "day", "sourceAndDay" ("day")
-              sortOrder: "desc" // optional, default "asc"
+              //sortOrder: "desc" // optional, default "asc"
             })
             .then(result => {
-              this.zone.run(() => {
-                console.log(JSON.stringify(result));
-                this.resultToShow = JSON.stringify(result);
-              });
+             var fitData = "";
+                   Object.keys(result[0]).forEach(function(key) {
+
+                  fitData += (key + ' : '  + result[0][key]+ "\n");
+                  })
+                  
+                this.resultToShow = fitData;
             })
             .catch(error => this.resultToShow = error);
       }
+
+     /* private updateStepsUI(result) {
+        console.log("schrittzaehler update: " + JSON.stringify(result));
+    
+        this.set("step_startDate", this.formatDate(result.startDate));
+        this.set("step_endDate", this.formatDate(result.endDate));
+        this.set("step_steps", result.steps);
+        this.set("step_distance", this.round(result.distance, 3) + " meter");
+      }*/
     
       startMonitoringData(dataType: string, unit: string): void {
         this.healthData.startMonitoring(
